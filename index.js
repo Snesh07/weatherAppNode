b4928a6ba249efed7ae0ab0de5f5f7af3507ead2
Sneha Sharma <87299768+SynonymOfLove@users.noreply.github.com>
2023-02-15 22:13:41 +0530

    fetched data from backend

--- index.js
+++ index.js
@@ -4,17 +4,31 @@ const requests = require("requests");
 
 const homeFile = fs.readFileSync("home.html", "utf-8");
 
+const replaceVal = (tempVal, orgVal) => {
+    let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+    temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+    temperature = temperature.replace("{%location%}", orgVal.name);
+    temperature = temperature.replace("{%country%}", orgVal.sys.country);
+    temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main)
+    return temperature;
+}
+
 const server = http.createServer((req, res) => {
     if (req.url == "/") {
-        requests("https://api.openweathermap.org/data/2.5/weather?q=Jhansi&appid=9f04e25a44db9a82f9a05f52922ea475",)
+        requests("https://api.openweathermap.org/data/2.5/weather?q=Jhansi&appid=9f04e25a44db9a82f9a05f52922ea475&units=metric",)
             .on("data", (chunk) => {
                 const objdata = JSON.parse(chunk);
-                console.log(chunk);
+                const arrData = [objdata];
+                // console.log(arrData[0].main.temp);
+                const realTimeData = arrData.map((val) => replaceVal(homeFile, val))
+                .join("");
+                res.write(realTimeData);
             })
             .on("end", (err) => {
                 if (err) return console.log('connection closed due to errors', err);
 
-                console.log("end");
+               res.end();
             });
     }
 });
